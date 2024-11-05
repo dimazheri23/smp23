5897e38b4109051144519b2e1ac5e6593cb10648
dimazheri23 <dimazheriyanto@gmail.com>
2024-11-05 09:53:36 +0700

    Add files via upload

---
+++ Config/koneksi.php
@@ -0,0 +1,6 @@
+<?php
+$konek=mysqli_connect("localhost","root","","db_sertifikasi");
+mysql_connect("localhost","root","") or die (mysql_error());
+mysql_select_db("db_sertifikasi") or die (mysql_error());
+
+?>
